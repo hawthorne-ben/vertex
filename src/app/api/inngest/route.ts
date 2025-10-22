43343332aed16f45cd9a1d52f391ff5201485738
Ben Hawthorne <hawthorneben@gmail.com>
2025-10-21 20:37:16 -0700

    test function and rules revision

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,13 @@
 import { serve } from 'inngest/next'
 import { inngest } from '@/inngest/client'
 import { parseIMU } from '@/inngest/functions/parse-imu'
+import { testFunction } from '@/inngest/functions/test-function'
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    parseIMU
+    parseIMU,
+    testFunction
   ]
 })
 
