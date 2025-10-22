4047e17360db37fb4558bf711fae7885eeec0c09
Ben Hawthorne <hawthorneben@gmail.com>
2025-10-21 19:35:15 -0700

    Fix Inngest client authentication - add eventKey and signingKey

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -2,6 +2,8 @@ import { Inngest } from 'inngest'
 
 export const inngest = new Inngest({ 
   id: 'vertex',
-  name: 'Vertex'
+  name: 'Vertex',
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY
 })
 
