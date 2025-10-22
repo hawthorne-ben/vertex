1b65565eb41c08219019df0b40bb654f1de087fa
Ben Hawthorne <hawthorneben@gmail.com>
2025-10-21 21:01:05 -0700

    Add minimal parseIMUTest function to test registration

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,13 @@
 import { serve } from 'inngest/next'
 import { inngest } from '@/inngest/client'
 import { testFunction } from '@/inngest/functions/test-function'
+import { parseIMUTest } from '@/inngest/functions/parse-imu-test'
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    testFunction
+    testFunction,
+    parseIMUTest
   ]
 })
 
