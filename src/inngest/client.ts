f35b721555ad149e85637c9d75b0e25c82c21116
Ben Hawthorne <hawthorneben@gmail.com>
2025-10-28 10:04:51 -0700

    deleted files

--- src/inngest/client.ts
+++
@@ -1,9 +0,0 @@
-import { Inngest } from 'inngest'
-
-export const inngest = new Inngest({ 
-  id: 'vertex',
-  name: 'Vertex',
-  eventKey: process.env.INNGEST_EVENT_KEY,
-  signingKey: process.env.INNGEST_SIGNING_KEY
-})
-
